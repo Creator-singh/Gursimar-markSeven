16627579deb927825365490d85b488265413363b
Creator-singh <64949453+Creator-singh@users.noreply.github.com>
2021-09-16 23:09:14 +0530

    finished the vulcanTrans

---
+++ README.md
@@ -0,0 +1,5 @@
+# vulcanTrans
+
+Are you a fan of Star Wars?  Did you know that the gibberish they say is an actual language. The Vulcan language was spoken by the Vulcans of the planet Vulcan. Vulcans are a fictional extraterrestrial humanoid species in the Star Trek franchise who originate from the planet Vulcan. 
+
+Use the translator to convert your text from English to Vulcan Language.
---
+++ app.js
@@ -0,0 +1,28 @@
+var btnTranslate = document.querySelector("#button-translate");
+var txtInput = document.querySelector("#text-input");
+var txtOutput = document.querySelector("#text-output");
+
+var serverURL = "https://api.funtranslations.com/translate/vulcan.json";
+
+
+function generateURL(inputText){
+    return serverURL + "?" + "text=" + inputText;
+}
+
+function errorHandler(error){
+    alert("Something went wrong! Try again later.")
+}
+
+function clickHandler(){
+    var inputText = txtInput.value;
+    fetch(generateURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            txtOutput.innerText = translatedText; 
+        })
+        .catch(errorHandler)
+};
+
+
+btnTranslate.addEventListener("click", clickHandler);
